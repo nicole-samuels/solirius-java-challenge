54cb202c90df219a3f03d526c9aac4449a2c304c
nicole-samuels <nicole.samuels@solirius.com>
2026-03-05 16:10:42 +0000

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,39 @@
+package org.example;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+    static void main() throws IOException {
+
+        final int port = 8080;
+        final ServerSocket server = new ServerSocket(port);
+        System.out.printf("Listening for connection on port %d ", port);
+
+        while (true){
+//            Read HTTP request
+//            Create a HTTP response
+            try {
+                Socket clientSocket =  server.accept();
+                System.out.println("Client connected!");
+                handleRequest(clientSocket);
+            } catch (IOException e) {
+                System.err.println("Connection Error: "+ e.getMessage());
+            }
+        }
+
+    }
+
+    private static void handleRequest(Socket clientSocket) {
+//        TODO: Read the request from clientSocket.getInputStream()
+//        TODO: Parse the route (e.g., GET /index.html)
+//        TODO: Write the HTTP headers and Write the html to the OutputStream
+
+    }
+
+
+}
